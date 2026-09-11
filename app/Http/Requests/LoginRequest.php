2aaf42f36a4f23cfbd9b8d8e90639a6ee1f0b4f5
anmerutu3203 <anmerutu3203@gmail.com>
2026-09-11 14:22:55 +0900

    feat: ログイン・ログアウト機能（一般ユーザー）を実装#3

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+
+class LoginRequest extends FortifyLoginRequest
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'メールアドレスを入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+    }
+}
